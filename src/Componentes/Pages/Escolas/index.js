2774560debb31d67178b9c03970ef307ea1ceb10
Amanda <amanda.simoes1707@gmail.com>
2022-03-28 23:38:26 -0300

    feat: estilizaçao da tela de listagem de escolas

--- src/Componentes/Pages/Escolas/index.js
+++ src/Componentes/Pages/Escolas/index.js
@@ -66,110 +66,135 @@ function Escolas() {
 
   return (
     <div>
-      <h3 class="titulo">Listagem de Escolas</h3>
-      <form onSubmit={enviar}>
-        <label> Escola: </label>
-        <input
-          class="escola"
-          placeholder="Informe uma escola"
-          type="text"
-          value={escola}
-          onChange={(e) => setEscola(e.target.value)}
-        />
-        <label> Fundamental 1 </label>
-        <input
-          type="checkbox"
-          checked={fund_ai}
-          onChange={(e) => setFund_ai(e.target.checked)}
-        />{" "}
-        <label> Fundamental 2 </label>
-        <input
-          type="checkbox"
-          checked={fund_af}
-          onChange={(e) => setFund_af(e.target.checked)}
-        />{" "}
-        <label> UF: </label>
-        <select value={uf} onChange={(e) => setUf(e.target.value)}>
-          <option value="undefined">Selecione</option>
-          <option value="AC">AC</option>
-          <option value="AL">AL</option>
-          <option value="AM">AM</option>
-          <option value="AP">AP</option>
-          <option value="BA">BA</option>
-          <option value="CE">CE</option>
-          <option value="DF">DF</option>
-          <option value="ES">ES</option>
-          <option value="GO">GO</option>
-          <option value="MA">MA</option>
-          <option value="MG">MG</option>
-          <option value="MS">MS</option>
-          <option value="MT">MT</option>
-          <option value="PA">PA</option>
-          <option value="PB">PB</option>
-          <option value="PE">PE</option>
-          <option value="PI">PI</option>
-          <option value="PR">PR</option>
-          <option value="RJ">RJ</option>
-          <option value="RN">RN</option>
-          <option value="RO">RO</option>
-          <option value="RR">RR</option>
-          <option value="RS">RS</option>
-          <option value="SC">SC</option>
-          <option value="SE">SE</option>
-          <option value="SP">SP</option>
-          <option value="TO">TO</option>
-        </select>
-        <label> Municipios: </label>
-        <select onChange={(e) => setMunicipio(e.target.value)}>
-          <option value="undefined">Selecione</option>
-          {cities.length >= 0
-            ? cities.map((city) => (
-                <option key={city.id} value={city.nome}>
-                  {city.nome}
-                </option>
-              ))
-            : null}
-        </select>
-        <label> Dependencias: </label>
-        <select
-          value={dependencia}
-          onChange={(e) => setDependencia(e.target.value)}
-        >
-          <option value="undefined">Selecione</option>
-          <option value="Municipal">Municipal</option>
-          <option value="Estadual">Estadual</option>
-          <option value="Privada">Privada</option>
-        </select>
-        <label> Localização: </label>
-        <select
-          value={localizacao}
-          onChange={(e) => setLocalizacao(e.target.value)}
-        >
-          <option value="undefined">Selecione</option>
-          <option value="Rural">Rural</option>
-          <option value="Urbana">Urbana</option>
-        </select>
-        <label> Educação Indigina: </label>
-        <input
-          type="checkbox"
-          checked={edu_indigena}
-          onChange={(e) => setEduIndigena(e.target.checked)}
-        />{" "}
-        <label> Acessibilidade: </label>
-        <input
-          type="checkbox"
-          checked={acessibilidade}
-          onChange={(e) => setAcessibilidade(e.target.checked)}
-        />{" "}
-        <label> AEE: </label>
-        <input
-          type="checkbox"
-          onChange={(e) => setAee(e.target.checked)}
-        />{" "}
-        <Button id="button" type="submit">
-          Enviar
-        </Button>
-      </form>
+      <label class="titulo">Escolas</label>
+      <div id="outIn">
+        <form onSubmit={enviar}>
+          <div id="first">
+            <label for="escola">
+              {" "}
+              Escola:
+              <input
+                class="escola"
+                id="escola"
+                placeholder="Informe uma escola"
+                type="text"
+                value={escola}
+                onChange={(e) => setEscola(e.target.value)}
+              />
+            </label>
+            <label>
+              {" "}
+              UF:
+              <select value={uf} onChange={(e) => setUf(e.target.value)}>
+                <option value="undefined">Selecione</option>
+                <option value="AC">AC</option>
+                <option value="AL">AL</option>
+                <option value="AM">AM</option>
+                <option value="AP">AP</option>
+                <option value="BA">BA</option>
+                <option value="CE">CE</option>
+                <option value="DF">DF</option>
+                <option value="ES">ES</option>
+                <option value="GO">GO</option>
+                <option value="MA">MA</option>
+                <option value="MG">MG</option>
+                <option value="MS">MS</option>
+                <option value="MT">MT</option>
+                <option value="PA">PA</option>
+                <option value="PB">PB</option>
+                <option value="PE">PE</option>
+                <option value="PI">PI</option>
+                <option value="PR">PR</option>
+                <option value="RJ">RJ</option>
+                <option value="RN">RN</option>
+                <option value="RO">RO</option>
+                <option value="RR">RR</option>
+                <option value="RS">RS</option>
+                <option value="SC">SC</option>
+                <option value="SE">SE</option>
+                <option value="SP">SP</option>
+                <option value="TO">TO</option>
+              </select>
+            </label>
+
+            <label>
+              {" "}
+              Municipios:
+              <select onChange={(e) => setMunicipio(e.target.value)}>
+                <option value="undefined">Selecione</option>
+                {cities.length >= 0
+                  ? cities.map((city) => (
+                      <option key={city.id} value={city.nome}>
+                        {city.nome}
+                      </option>
+                    ))
+                  : null}
+              </select>
+            </label>
+
+            <label>
+              {" "}
+              Dependencias:
+              <select
+                value={dependencia}
+                onChange={(e) => setDependencia(e.target.value)}
+              >
+                <option value="undefined">Selecione</option>
+                <option value="Municipal">Municipal</option>
+                <option value="Estadual">Estadual</option>
+                <option value="Privada">Privada</option>
+              </select>
+            </label>
+
+            <label>
+              {" "}
+              Localização:
+              <select
+                value={localizacao}
+                onChange={(e) => setLocalizacao(e.target.value)}
+              >
+                <option value="undefined">Selecione</option>
+                <option value="Rural">Rural</option>
+                <option value="Urbana">Urbana</option>
+              </select>
+            </label>
+          </div>
+          <div id="second">
+            <label> Fundamental 1 </label>
+            <input
+              type="checkbox"
+              checked={fund_ai}
+              onChange={(e) => setFund_ai(e.target.checked)}
+            />{" "}
+            <label> Fundamental 2 </label>
+            <input
+              type="checkbox"
+              checked={fund_af}
+              onChange={(e) => setFund_af(e.target.checked)}
+            />{" "}
+            <label> Educação Indigina: </label>
+            <input
+              type="checkbox"
+              checked={edu_indigena}
+              onChange={(e) => setEduIndigena(e.target.checked)}
+            />{" "}
+            <label> Acessibilidade: </label>
+            <input
+              type="checkbox"
+              checked={acessibilidade}
+              onChange={(e) => setAcessibilidade(e.target.checked)}
+            />{" "}
+            <label> AEE: </label>
+            <input
+              type="checkbox"
+              onChange={(e) => setAee(e.target.checked)}
+            />{" "}
+            <Button id="button" type="submit">
+              Enviar
+            </Button>
+          </div>
+        </form>
+      </div>
 
       <div class="resultado">
         {typeof result === "string"
@@ -182,14 +207,8 @@ function Escolas() {
                   <Link to={"/detalhe/" + element.cod_escola}>
                     {element.nome_escola}
                   </Link>{" "}
+                  {element.municipio} - {element.uf}
                 </div>
-                <div> UF: {element.uf} </div>
-                {element.endereco != null ? (
-                  <div> Endereço: {element.endereco} </div>
-                ) : (
-                  ""
-                )}
-                <br />
               </div>
             ))}
       </div>
